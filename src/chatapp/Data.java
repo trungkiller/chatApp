f2e974a54b6f0a2d260433846a91217222f14ada
Nguyen Trung <trungpltb@gmail.com>
2020-03-06 21:41:09 +0700

    chatApp

---
+++ src/chatapp/Data.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapp;
+
+
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+
+public class Data implements Serializable{
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+    
+    public String toString() {
+        return name;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+    
+    private String status;
+    private String name;
+    private String nameUser;
+    private ImageIcon image;
+    private byte [] file;
+ }
